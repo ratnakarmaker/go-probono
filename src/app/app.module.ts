4580384c6dc16b9f9f2ccbb082945250149d9549
ratnakarmaker <ratnarani.cse@gmail.com>
2023-03-02 12:37:04 +0600

    project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderNavbarComponent } from './components/header-navbar/header-navbar.component';
+import { HeaderTopComponent } from './components/header-top/header-top.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderNavbarComponent,
+    HeaderTopComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
