ab44735fc807c93f878f16db0ded0b9a12caf035
arif <arif@eatlbd.com>
2023-04-16 20:31:58 +0600

    fix

--- src/app/entrance/lady-justice/main-laws/main-laws.component.ts
+++ src/app/entrance/lady-justice/main-laws/main-laws.component.ts
@@ -2,60 +2,47 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from 'src/app/services/api/api.service';
 
-interface Law {
-  header: string;
-  subtitle: string;
-  description: string;
-  id?: number;
-}
-
 @Component({
   selector: 'app-main-laws',
   templateUrl: './main-laws.component.html',
   styleUrls: ['./main-laws.component.scss'],
 })
 export class MainLawsComponent {
-  lawList: Law[] = [
-    {
-      id: 1,
-      header: 'অর্থ আইন, ১৯৯৮',
-      subtitle: '১৯৯৮ সনের ১৪ নং আইন',
-      description:
-        'সরকারের আর্থিক প্রস্তাবাবলী কার্যকরকরণ এবং কতিপয় আইন সংশোধনকল্পে প্রণীত আইন',
-    },
-  ];
+  lawList: any[] = [];
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private api: ApiService
+    protected api: ApiService
   ) {}
 
   ngOnInit(): void {
     this.getMainLaws();
   }
 
-  lawDescription(law: Law) {
+  lawDescription(law: any) {
     this.router.navigate([`../main-laws/${law?.id}`], {
       relativeTo: this.route,
     });
   }
 
-  lawListView(law: Law) {
+  lawListView(law: any) {
     this.router.navigate([`../law-list`], {
       relativeTo: this.route,
-      queryParams: { major_id: law?.id },
+      queryParams: { law: law?.id },
     });
   }
 
-  advocateListView(law: Law) {
+  advocateListView(law: any) {
     this.router.navigate(['../../advocates'], {
       relativeTo: this.route,
-      queryParams: { major_id: law?.id },
+      queryParams: { law: law?.id },
     });
   }
 
   getMainLaws() {
-    this.api.list('LAW_LIST_API').subscribe((response: any) => {});
+    this.api.list('LAW_LIST_API').subscribe((response: any) => {
+      this.lawList = response;
+    });
   }
 }
