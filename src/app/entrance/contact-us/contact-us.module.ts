c5520974d7225a023ddefaf7b733fe05800ce902
arif <arif@eatlbd.com>
2023-04-16 22:45:11 +0600

    fix

--- src/app/entrance/contact-us/contact-us.module.ts
+++ src/app/entrance/contact-us/contact-us.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CallerComponent } from './caller/caller.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: CallerComponent,
+    title: '',
+  },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [CallerComponent],
+  imports: [CommonModule, RouterModule.forChild(routes)],
 })
-export class ContactUsModule { }
+export class ContactUsModule {}
