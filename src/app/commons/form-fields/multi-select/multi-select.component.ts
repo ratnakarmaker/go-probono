dc144173ecaa47564271e208aa4984ad77710fe5
Arif <arifur@eatlbd.com>
2024-02-22 19:21:10 +0600

    fix

--- src/app/commons/form-fields/multi-select/multi-select.component.ts
+++ src/app/commons/form-fields/multi-select/multi-select.component.ts
@@ -1,10 +1,18 @@
-import { Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-multi-select',
   templateUrl: './multi-select.component.html',
-  styleUrls: ['./multi-select.component.scss']
+  styleUrls: ['./multi-select.component.scss'],
 })
 export class MultiSelectComponent {
   @Input() control: AbstractControl<any, any> | any = null;
@@ -19,9 +27,12 @@ export class MultiSelectComponent {
 
   @HostListener('document:click', ['$event'])
   onGlobalClick(event: any): void {
-    if (this.input_field?.nativeElement?.contains(event?.target) || this.select_options?.nativeElement?.contains(event?.target)){
+    if (
+      this.input_field?.nativeElement?.contains(event?.target) ||
+      this.select_options?.nativeElement?.contains(event?.target)
+    ) {
       this.option_class = 'show-option';
-    }else{
+    } else {
       this.option_class = 'hide-option';
     }
   }
@@ -29,26 +40,40 @@ export class MultiSelectComponent {
   @Output() controlChange: EventEmitter<any> = new EventEmitter<any>();
 
   protected search_text: string = '';
-  protected input_values: {[key: string]: any} = {};
+  protected input_values: Map<string, any> = new Map();
   protected option_class: 'show-option' | 'hide-option' = 'hide-option';
+  protected selected_list: any[] = [];
 
-  selectedList(){
-    return Object.values(this.input_values);
+  optionShower(opt: any) {
+    return (
+      !this.search_text ||
+      opt?.[this.view_control]
+        ?.toLowerCase()
+        ?.includes(this.search_text.toLowerCase())
+    );
   }
 
-  optionShower(opt: any){
-    return !this.search_text || opt?.[this.view_control]?.toLowerCase()?.includes(this.search_text.toLowerCase());
-  }
-
-  setValue(option: any){
-    if(option?.[this.value_control] in this.input_values){
-      delete this.input_values?.[option?.[this.value_control]];
-    }else{
-      this.input_values = {
-        ...this.input_values,
-        [option?.[this.value_control]]: option
-      }
+  setValue(option: any) {
+    if (this.input_values.has(`${option?.[this.value_control]}`)) {
+      this.input_values.delete(`${option?.[this.value_control]}`);
+    } else {
+      this.input_values.set(`${option?.[this.value_control]}`, option);
     }
-    this.controlChange.emit(Object.keys(this.input_values))
+    this.selected_list = Array.from(this.input_values.values());
+    if(this.control instanceof AbstractControl){
+      this.control.setValue(Array.from(this.input_values.keys()));
+    }else{
+      this.controlChange.emit(Array.from(this.input_values.keys()));
+    }
+  }
+
+  clearAll(){
+    this.input_values.clear();
+    this.selected_list = [];
+    if(this.control instanceof AbstractControl){
+      this.control.setValue([]);
+    }else{
+      this.controlChange.emit([]);
+    }
   }
 }
